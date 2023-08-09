11ab1b19e6efdbdb5c4d11149a702a65921c356a
Francis Ozoka <fozoka@turing.une.edu.au>
2023-08-09 20:58:13 +1000

    Added minmax funciton

--- Proposed solutions-20230808/tictactoe_app.py
+++ Proposed solutions-20230808/tictactoe_app.py
@@ -9,11 +9,17 @@ from agent_programs import agent_program_minimax, agent_program_minimax_alpha_be
 from agent_programs import agent_program_mcts
 
 if __name__ == '__main__':
+    # Creating the transposition table
+    tt = TicTacToeTTable(instance_id='6879eb26-1ba1-11ee-8577-9e02763478a3')
+    print("Initialised a Transposition table with instance id {0}".format(tt.get_instance_id()))
+
+    wrapped_minimax_tt = lambda perc, act: agent_program_optimised_minimax(perc, act, tt, max_depth=4)
+
     # Creating the two players
     # To change their behaviour, change the second parameter
     # of the constructor with the desired agent program function
-    player_X = TicTacToePlayer('X', agent_program_minimax)
-    player_O = TicTacToePlayer('O', agent_program_random)
+    player_X = TicTacToePlayer('X', wrapped_minimax_tt)
+    player_O = TicTacToePlayer('O', lambda perc, act: agent_program_minimax_alpha_beta(perc, act, 10))
 
     # DO NOT EDIT THE FOLLOWING INSTRUCTIONS!
     environment = TicTacToeGameEnvironment()
@@ -21,3 +27,4 @@ if __name__ == '__main__':
     environment.add_player(player_O)
 
     game = TicTacToeGame(player_X, player_O, environment)
+
--- minimax_functions.py
+++ minimax_functions.py
@@ -2,10 +2,60 @@ from tictactoe_game_environment import TicTacToeGameEnvironment as gm
 from une_ai.models import GraphNode
 
 def minimax(node, player, depth):
-    return 0, None
+    print('{0}'.format(node))
+    best_move = None
+    gs = node.get_state()
+    player_turn = gs['player-turn']
+    is_maximizing = player_turn == player
+
+    if is_maximizing:
+        best_payoff = float('-inf')
+    else:
+        best_payoff = float('+inf')
+    
+    if gm.is_terminal(gs) or depth <= 0:
+        best_payoff = gm.payoff(gs, player)
+        return best_payoff, best_move
+    
+    actions = gm.get_legal_actions(gs)
+
+    for action in actions:
+        future_state = gm.transition_result(gs, action)
+        (payoff, _) = minimax(GraphNode(future_state, node, action, 1), player, depth-1)
+
+        if (is_maximizing and payoff > best_payoff) \
+            or not is_maximizing and payoff < best_payoff:
+            best_payoff = payoff
+            best_move = action
+    return (best_payoff, best_move)
 
 def minimax_alpha_beta(node, player, alpha, beta, depth):
-    return 0, None
+    print('{0}'.format(node))
+    best_move = None
+    gs = node.get_state()
+    player_turn = gs['player-turn']
+    is_maximizing = player_turn == player
+
+    if is_maximizing:
+        best_payoff = float('-inf')
+    else:
+        best_payoff = float('+inf')
+    
+    if gm.is_terminal(gs) or depth <= 0:
+        best_payoff = gm.payoff(gs, player)
+        return best_payoff, best_move
+    
+    actions = gm.get_legal_actions(gs)
+
+    for action in actions:
+        future_state = gm.transition_result(gs, action)
+        (payoff, _) = minimax(GraphNode(future_state, node, action, 1), player, depth-1)
+
+        if (is_maximizing and payoff > best_payoff) \
+            or not is_maximizing and payoff < best_payoff:
+            best_payoff = payoff
+            best_move = action
+    return (best_payoff, best_move)
 
 def optimised_minimax(node, player, tt, depth):
     return 0, None
